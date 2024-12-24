d00ddac950a1a2f6077e5bd6e9a7962edfe14651
Rickmwasofficial <rickmwasofficial@gmail.com>
2024-12-24 13:43:41 +0300

    first play

---
+++ playground-2/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var Services;
+(function (Services) {
+    Services["code"] = "We help write scalable code for your organization";
+    Services["design"] = "We help design the best user interfaces for your websites";
+    Services["model"] = "We help train the best model for cv tasks";
+})(Services || (Services = {}));
+let model = Services.model;
+console.log(model);
---
+++ playground-2/app.ts
@@ -0,0 +1,9 @@
+enum Services {
+    code = 'We help write scalable code for your organization',
+    design = 'We help design the best user interfaces for your websites',
+    model = 'We help train the best model for cv tasks'
+}
+
+let model = Services.model
+
+console.log(model)
